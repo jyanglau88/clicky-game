d2ee4accb23fe8fda2e6b96d7018a794de0d4674
jyanglau88 <jyanglau88@gmail.com>
2018-10-23 21:10:42 -0700

    additional updates

--- src/App.js
+++ src/App.js
@@ -5,23 +5,22 @@ import Header from "./components/Header";
 import CardContainer from "./components/CardContainer"
 
 class App extends Component {
-  // setting the state
+  // starting score
   state = {
     count: 0,
     topScore: 0
   }
 
-  // update the current score count in the nav
+  // update scoreboard
   updateCurrentScore = (newCount) => {
-    // set the new count as the count
+    // new score update
     this.setState({count: newCount});
   }
 
-  // update the top score in the nav
+  // display top score
   updateTopScore = (newTop) => {
-    // if the new top score is higher than the current
+    // new score if greater than top score update
     if (newTop > this.state.topScore) {
-      // then set the state but you have to subtract 1
       this.setState({topScore: newTop - 1})
     }
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = props => (
     <div className="container text-center" id="header-container">
-        <h1 id="logo"><b>React Clicky Game</b></h1>
+        <h1 id="logo"><b>Pokémon Memory Game!</b></h1>
         <br/>
-        <h5 className="subtitle">Instructions:</h5>
+        <h5 className="subtitle">Are you ready to become a Pokémon Master?</h5>
         <br/>
-        <h5 className="subtitle">Click on an friend to get started! After each click, the friends will shuffle. <br/> The goal is to select each friend once, so everyone gets a turn!</h5>
+        <h5 className="subtitle">Click on a starter Pokémon to get started! After each click everyone will shuffle. <br/> The goal is to select each Pokémon once, so everyone gets a turn!</h5>
     </div>
 )
 
