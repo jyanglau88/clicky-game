72100f213185584a0f5e68e13df7a6c71bda6a12
jyanglau88 <jyanglau88@gmail.com>
2018-10-24 19:34:16 -0700

    change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,8 @@ import React from 'react';
 const Header = props => (
     <div className="container text-center" id="header-container">
         <h1 id="logo"><b>Pokémon Memory Game!</b></h1>
-        <p>
-            <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Instructions:</a>
-        </p>
-        <div className="collapse" id="collapseExample">
-            <div className="card card-body instructions">
-                Are you ready to become a Pokémon Master? To begin your journey, click on a starter Pokémon to get started! After each click everyone will shuffle. The goal is to select each Pokémon once, so everyone gets a turn!
-  </div>
-        </div>
+        <br/>
+        <p className="instructions alert alert-danger">Are you ready to become a Pokémon Master? To begin your journey, click on a starter Pokémon to get started! After each click everyone will shuffle. The goal is to select each Pokémon once, so everyone gets a turn!</p>
     </div>
 )
 
