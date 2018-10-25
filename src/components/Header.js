a5ba18240495284be1961990fb95bae077abc10c
jyanglau88 <jyanglau88@gmail.com>
2018-10-24 19:36:44 -0700

    more updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = props => (
-    <div className="container text-center" id="header-container">
+    <div className="container text-center jumbotron" id="header-container">
         <h1 id="logo"><b>Pokémon Memory Game!</b></h1>
         <br/>
         <p className="instructions alert alert-danger">Are you ready to become a Pokémon Master? To begin your journey, click on a starter Pokémon to get started! After each click everyone will shuffle. The goal is to select each Pokémon once, so everyone gets a turn!</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = props => (
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+    <nav className="navbar navbar-dark bg-dark">
         <h3 className="navbar-brand" href="/" id="logo">Pokémon React Clicky Game</h3>
         <h5 className="ml-auto mt-2" id="scoreboard">Score: {props.score} | Top Score: {props.top} </h5>
     </nav>
