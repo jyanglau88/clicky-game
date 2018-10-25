e17dde505ac3a77efcbcd7bea4f7c6bb69120de7
jyanglau88 <jyanglau88@gmail.com>
2018-10-24 19:41:13 -0700

    more udpate

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = props => (
     <div className="col-md-3">
         <div className="card mb-5">
-            <img className="card-img-top" src={props.src} alt={props.alt} onClick={props.handleClick} id={props.id}/>
+            <img className="card-img-top rounded-circle" src={props.src} alt={props.alt} onClick={props.handleClick} id={props.id}/>
         </div>
     </div>
 )
